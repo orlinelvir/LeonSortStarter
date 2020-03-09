949f6bf714657492b97ab6e676821dbc7b2c49dc
Orlin Elvir <orlin.elvir@gmail.com>
2020-03-09 11:32:30 -0600

    IterativeMergeSort en prueba

--- src/edu/ujcv/estructuras/iterativeMergeSort/IterativeMergeSort.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/IterativeMergeSort.java
@@ -1,10 +1,8 @@
 package edu.ujcv.estructuras.iterativeMergeSort;
 import java.util.Arrays;
-public class IterativeMergeSort
-{
-    public static void IterativeMerge(int[] A, int[] temp, int from, int mid, int to)
-    {
-        int k = from, i = from, j = mid +1;
+public class IterativeMergeSort {
+    public static void IterativeMerge(int[] A, int[] temp, int from, int mid, int to) {
+        int k = from, i = from, j = mid + 1;
         while (i <= mid && j <= to) {
             if (A[i] < A[j]) {
                 temp[k++] = A[i++];
@@ -12,22 +10,20 @@ public class IterativeMergeSort
                 temp[k++] = A[j++];
             }
         }
-    while (i<= mid){
-        temp[k++] = A[i++];
+        while (i <= mid) {
+            temp[k++] = A[i++];
+        }
+        for (i = from; i <= to; i++) {
+            A[i] = temp[i];
+        }
     }
-    for (i = from; i <= to; i++){
-        A[i] = temp[i];
-    }
-}
-    public static void mergesort(int[] A)
-    {
+
+    public static void mergesort(int[] A) {
         int low = 0;
         int high = A.length - 1;
         int[] temp = Arrays.copyOf(A, A.length);
-        for (int m = 1; m <= high - low; m = 2*m)
-        {
-            for (int i = low; i < high; i += 2*m)
-            {
+        for (int m = 1; m <= high - low; m = 2 * m) {
+            for (int i = low; i < high; i += 2 * m) {
                 int from = i;
                 int mid = i + m - 1;
                 int to = Integer.min(i + 2 * m - 1, high);
@@ -35,3 +31,11 @@ public class IterativeMergeSort
             }
         }
     }
+    public static void main(String[] args)
+    {
+        int[] A = { 69, 21, 07, 14, 10, 11, 15 };
+        System.out.println("Original Array : " + Arrays.toString(A));
+        mergesort(A);
+        System.out.println("Modified Array : " + Arrays.toString(A));
+    }
+}
