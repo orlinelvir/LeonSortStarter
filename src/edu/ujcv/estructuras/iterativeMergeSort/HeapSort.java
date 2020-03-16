66a817ef906ba0b2f532f7737ff60e4dd6b02ee7
Orlin Elvir <orlin.elvir@gmail.com>
2020-03-16 15:32:26 -0600

    HeapSort Cambio de tiempo en ejecucion a NanoSegundos

--- src/edu/ujcv/estructuras/iterativeMergeSort/HeapSort.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/HeapSort.java
@@ -5,25 +5,30 @@ import java.util.PriorityQueue;
 public class HeapSort implements SortTester {
     @Override
     public long sort(int[] array) {
-        long start = System.currentTimeMillis();
-        heapSort(array);
+        long start = System.nanoTime();
 
-        long end = System.currentTimeMillis();
+        heapSort(array);
+        long end = System.nanoTime();
 
         return end - start;
     }
 
-    public static int[] heapSort(int[] array) {
 
-        // cambiar priority queue por su propia implementacion!!
-        PriorityQueue <Integer> queue = new PriorityQueue<>();
-        for (Integer i: array) {
-            queue.add(i);
+    public void heapSort(int[] array) {
+
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            Flotar(array, n, i);
+            //Todo
         }
-        for (int i = 0; i < array.length; i++) {
-            array[i] = queue.remove();
+
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            Flotar(array, i, 0);
         }
-        return array;
     }
 
-}
+
