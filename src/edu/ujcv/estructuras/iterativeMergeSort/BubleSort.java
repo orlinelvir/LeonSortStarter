0c2709dbf655008d0f20d4ad128cbd473ca0844d
Orlin Elvir <orlin.elvir@gmail.com>
2020-03-16 15:27:06 -0600

    Buble sort de milisegundos a nano segundos

--- src/edu/ujcv/estructuras/iterativeMergeSort/BubleSort.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/BubleSort.java
@@ -3,21 +3,19 @@ package edu.ujcv.estructuras.iterativeMergeSort;
 public class BubleSort implements SortTester {
     @Override
     public long sort(int[] array) {
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         bubleSort(array);
-
-        long end = System.currentTimeMillis();
-
+        long end = System.nanoTime();
         return end - start;
     }
 
     public static int[] bubleSort(int[] a){
         for (int i = 0; i < a.length ; i++) {
             for (int j = 0; j < a.length; j++) {
-                if(a[i] > a[j]){
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                if(a[j] > a[i]){
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
                 }
             }
         }
--- src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
@@ -14,6 +14,7 @@ public class Main {
         }
 
         elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
+        System.out.println("SHUFFLE SORT");
         System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
     }
 }
