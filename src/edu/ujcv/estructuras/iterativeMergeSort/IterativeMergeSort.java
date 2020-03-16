55a5a8bf698e7f654752ec0cdebbc77614ee3c13
Orlin Elvir <orlin.elvir@gmail.com>
2020-03-16 16:17:29 -0600

    Corrigiendo errores

--- src/edu/ujcv/estructuras/iterativeMergeSort/IterativeMergeSort.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/IterativeMergeSort.java
@@ -1,41 +1,84 @@
 package edu.ujcv.estructuras.iterativeMergeSort;
 import java.util.Arrays;
-public class IterativeMergeSort {
-    public static void IterativeMerge(int[] A, int[] temp, int from, int mid, int to) {
-        int k = from, i = from, j = mid + 1;
-        while (i <= mid && j <= to) {
-            if (A[i] < A[j]) {
-                temp[k++] = A[i++];
-            } else {
-                temp[k++] = A[j++];
-            }
-        }
-        while (i <= mid) {
-            temp[k++] = A[i++];
-        }
-        for (i = from; i <= to; i++) {
-            A[i] = temp[i];
-        }
+import java.util.*;
+import java.lang.*;
+
+public class IterativeMergeSort implements SortTester {
+    public long sort(int[] array) {
+        long start = System.currentTimeMillis();
+        new IterativeMergeSort(array);
+
+        long end = System.currentTimeMillis();
+
+        return end - start;
     }
 
-    public static void mergesort(int[] A) {
-        int low = 0;
-        int high = A.length - 1;
-        int[] temp = Arrays.copyOf(A, A.length);
-        for (int m = 1; m <= high - low; m = 2 * m) {
-            for (int i = low; i < high; i += 2 * m) {
-                int from = i;
-                int mid = i + m - 1;
-                int to = Integer.min(i + 2 * m - 1, high);
-                IterativeMerge(A, temp, from, mid, to);
+    public static void IterativeMerge(String args[])
+    {
+        int[] nums = {7,8,9,4,6,5,2,4,3,1,6,8,7,9,2,4,3,5,6};
+
+        printArray(nums);
+
+        mergeSort(nums);
+
+        printArray(nums);
+    }
+
+    public static void printArray(int[] nums) {
+        for(Integer num : nums)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void mergeSort(int[] nums) {
+        if(nums == null) {
+            return;
+        }
+
+        if(nums.length > 1) {
+            int mid = nums.length / 2;
+
+            // Split left part
+            int[] left = new int[mid];
+            for(int i = 0; i < mid; i++) {
+                left[i] = nums[i];
+            }
+
+            // Split right part
+            int[] right = new int[nums.length - mid];
+            for(int i = mid; i < nums.length; i++) {
+                right[i - mid] = nums[i];
+            }
+
+            mergeSort(left);
+            mergeSort(right);
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            // Merge left and right arrays
+            while(i < left.length && j < right.length) {
+                if(left[i] < right[j]) {
+                    nums[k] = left[i];
+                    i++;
+                } else {
+                    nums[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Collect remaining elements
+            while(i < left.length) {
+                nums[k] = left[i];
+                i++; k++;
+            }
+
+            while(j < right.length) {
+                nums[k] = right[j];
+                j++; k++;
             }
         }
-    }
-    public static void main(String[] args)
-    {
-        int[] A = { 69, 21, 07, 14, 10, 11, 15 };
-        System.out.println("Original Array : " + Arrays.toString(A));
-        mergesort(A);
-        System.out.println("Modified Array : " + Arrays.toString(A));
     }
 }
--- src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
@@ -5,16 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
-        ShuffleSort s = new ShuffleSort();
-        elementos = s.shuffleSort(elementos);
+        int[] elementos = {0,55,67,17,20,10,99,27,9,2,1};
+
+
+        IterativeMergeSort s = new IterativeMergeSort();
+//        s.cycleSort(elementos);
+
+//        MergeSort s = new MergeSort();
+//        s.mergeSort(elementos);
+
+//        HeapSort s = new HeapSort();
+//        s.heapSort(elementos);
+
+//        BubleSort s = new BubleSort();
+//        s.bubleSort(elementos);
+
 
         for (int i = 0; i < elementos.length; i++) {
             System.out.println(elementos[i]);
         }
 
-        elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
-        System.out.println("SHUFFLE SORT");
-        System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
+        System.out.println("\n\nTarda "+ s.sort(elementos)+ " Nanosegundos");
     }
 }
