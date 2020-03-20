51240d109f91e4cb2b17029fe68f74f3c3c52802
Orlin Elvir <orlin.elvir@gmail.com>
2020-03-20 14:52:42 -0600

    Mejoras del codigo 3

--- src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
+++ src/edu/ujcv/estructuras/iterativeMergeSort/Main.java
@@ -3,8 +3,6 @@ package edu.ujcv.estructuras.iterativeMergeSort;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
         int[] elementos = {0,55,67,17,20,10,99,27,9,2,1};
 
         IterativeMergeSort it = new IterativeMergeSort();
@@ -13,11 +11,11 @@ public class Main {
         MergeSort m = new MergeSort();
         m.mergeSort(elementos);
 
-          HeapSort h = new HeapSort();
-          h.heapSort(elementos);
+        HeapSort h = new HeapSort();
+        h.heapSort(elementos);
 
-          BubleSort b = new BubleSort();
-          b.bubleSort(elementos);
+        BubleSort b = new BubleSort();
+        b.bubleSort(elementos);
 
         System.out.println("\t\t\tPROYECTO ESTRUCTURAS DE DATOS\t\t\t");
         System.out.println("\t\t\tORLIN ELVIR\t\t\t");
@@ -28,7 +26,7 @@ public class Main {
         for (int i = 0; i < elementos.length; i++) {
             System.out.println(elementos[i]);
         }
-        System.out.println("\n \t\tEl  Algoritmo Iterative Merge Sort tarda "+ it.sort(elementos)+ " Nanosegundos en ordenar el Arreglo");
+        System.out.println("\n \t\tEl Algoritmo Iterative Merge Sort tardó "+ it.sort(elementos)+ " Nanosegundos en ordenar el Arreglo");
         System.out.println("\t\tEl Algoritmo MergeSort tardó: " + m.sort(elementos) + " Nanosegundos en ordenar el Arreglo.");
         System.out.println("\t\tEl Algoritmo HeapSort tardó: " + h.sort(elementos) + " Nanosegundos en ordenar el Arreglo.");
         System.out.println("\t\tEl Algoritmo BubbleSort tardó: " + b.sort(elementos) + " Nanosegundos en ordenar el Arreglo.");
